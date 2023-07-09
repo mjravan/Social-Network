da4628c4d3047a29999e5d357433fba18da37fa7
Majed <majed79a@gmail.com>
2023-07-10 00:38:40 +0330

    added Profile page and features

--- social_network/account/views.py
+++ social_network/account/views.py
@@ -63,3 +63,8 @@ class UserLogoutView(LoginRequiredMixin, View):
         messages.success(request, 'you logged out successfully', 'success')
         return redirect('home:home')
 
+
+class UserProfileView(LoginRequiredMixin, View):
+    def get(self, request, user_id):
+        user = User.objects.get(pk=user_id)
+        return render(request, 'account/profile.html', {'user': user})
