eeef4b239e79952892dfd01882c3ac8170084599
Majed <majed79a@gmail.com>
2023-07-12 13:20:08 +0330

    added password-reset feature

--- social_network/social_network/settings.py
+++ social_network/social_network/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
+import django.core.mail.backends.smtp
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -132,3 +134,12 @@ AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
     'account.authenticate.EmailBackend'
 ]
+
+# Google account
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'majed79a@gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_PASSWORD = 'iqshwfwcscobnpvn'
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = 'MJ Website'
